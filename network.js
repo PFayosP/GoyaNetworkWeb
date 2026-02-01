827a0bf80cc54b873af930aeb3b62fd45dcbd577
Paula Fayos Pérez <pfayosperez@gmail.com>
2026-02-01 19:50:06 +0100

    nationality/profession panel update

--- network.js
+++ network.js
@@ -650,8 +650,12 @@ window.renderFilterPanel = function ({ professionFilter, nationalityFilter, matc
   const items = (matchingNodeIds || [])
     .map(id => nodes.get(id))
     .filter(Boolean)
-    .map(n => ({ id: n.id, name: (n.label || n.id) }))
-    .sort((a, b) => a.name.localeCompare(b.name));
+    .map(n => ({ id: n.id, name: n.id }))
+    .sort((a, b) => {
+      const ka = __gnSurnameKeyForSort(a.name);
+      const kb = __gnSurnameKeyForSort(b.name);
+      if (ka !== kb) return ka.localeCompare(kb);
+      return a.name.localeCompare(b.name);
 
   const count = items.length;
 
